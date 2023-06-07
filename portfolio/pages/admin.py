b47593ffebd6ede47f54f252cd4be078ff3c11fc
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 02:30:22 +0330

    ✨ feat: added About SkillCategory, Skill, ContactUs and Project to admin

--- portfolio/pages/admin.py
+++ portfolio/pages/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from portfolio.pages.models import Page
+from portfolio.pages.models import Page, About, SkillCategory, Skill, ContactUs, Project
 
 admin.site.register(Page)
+admin.site.register(About)
+admin.site.register(SkillCategory)
+admin.site.register(Skill)
+admin.site.register(ContactUs)
+admin.site.register(Project)
