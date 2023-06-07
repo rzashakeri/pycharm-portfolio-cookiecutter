01383d82775ca1c784bffc51da3444924dce9cd6
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 03:08:40 +0330

    ✨ feat: added HomeView

--- portfolio/pages/views.py
+++ portfolio/pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from portfolio.pages.models import SiteSettings, Page
+
+
+class HomeView(View):
+    def get(self, request):
+        page = Page.objects.first()
+        portfolio = SiteSettings.objects.first()
+        context = {
+            "page": page,
+            "portfolio": portfolio
+        }
+        return render(request, "pages/index.html", context=context)
