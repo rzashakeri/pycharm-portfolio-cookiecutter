344e5c9046fec226741bea6c5f6ef4c6676f2f7f
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 02:16:15 +0330

    ✨ feat: added pages app to LOCAL_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,6 +88,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "portfolio.users",
+    "portfolio.pages",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
