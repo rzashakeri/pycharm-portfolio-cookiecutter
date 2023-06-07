46776d6c5eadde78a133ec7a19a84f64c0700e3d
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 02:21:18 +0330

    ✨ feat: added about model

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=200)
     icon = models.ImageField()
+
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
