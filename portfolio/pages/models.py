d536423c39c5baa7557a41483e97b0c3e16bfaf4
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 02:24:36 +0330

    ✨ feat: added skill and skill category model

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import CASCADE
 
 
 class Page(models.Model):
@@ -9,3 +10,14 @@ class Page(models.Model):
 class About(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+
+
+class SkillCategory(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class Skill(models.Model):
+    category = models.ForeignKey(SkillCategory, on_delete=CASCADE)
+    name = models.CharField(max_length=50)
+    icon = models.ImageField()
