51bd3eab153f31d68b2c0372276150f6228efb46
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 03:14:02 +0330

    ♻️ refactor: added str method to SkillCategory model

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -24,7 +24,6 @@ class About(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
 
-
     def __str__(self):
         return self.title
 
@@ -32,6 +31,9 @@ class About(models.Model):
 class SkillCategory(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Skill(models.Model):
