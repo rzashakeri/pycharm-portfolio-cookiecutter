365b14fcc4aaa7a42f0f1b21a5aafca93274c05e
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 15:49:29 +0330

    ✨ feat: added about url

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from portfolio.pages.views import HomeView
+from portfolio.pages.views import HomeView, AboutView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='Home')
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
