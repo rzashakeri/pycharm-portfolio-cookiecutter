4f694e39c3f29acffa15e374248ef76f34053e6d
Reza shakeri <rzashakeri@gmail.com>
2023-06-08 15:48:59 +0330

    ✨ feat: added AboutView

--- portfolio/pages/views.py
+++ portfolio/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from portfolio.pages.models import SiteSettings, Page
+from portfolio.pages.models import SiteSettings, Page, About
 
 
 class HomeView(View):
@@ -13,3 +13,16 @@ class HomeView(View):
             "portfolio": portfolio
         }
         return render(request, "pages/index.html", context=context)
+
+
+class AboutView(View):
+    def get(self, request):
+        page = Page.objects.get(title="about.html")
+        portfolio = SiteSettings.objects.first()
+        about = About.objects.first()
+        context = {
+            "page": page,
+            "portfolio": portfolio,
+            "about": about
+        }
+        return render(request, "pages/about.html", context=context)
