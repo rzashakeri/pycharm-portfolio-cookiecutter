f9c521aeaf510f4237894bd6a085f0fff98069f1
Reza shakeri <rzashakeri@gmail.com>
2023-06-11 00:04:54 +0330

    ✨ feat: added projects url

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from portfolio.pages.views import HomeView, AboutView, ContactUsView
+from portfolio.pages.views import HomeView, AboutView, ContactUsView, ProjectsView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('home/', HomeView.as_view()),
     path('about-us/', AboutView.as_view(), name='about'),
+    path('projects/', ProjectsView.as_view(), name='projects'),
     path('contact-us/', ContactUsView.as_view(), name='contact_us'),
 ]
