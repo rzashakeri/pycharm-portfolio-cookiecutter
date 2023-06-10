d95da84a10155d64bf77cfe7992cb78014f5f466
Reza shakeri <rzashakeri@gmail.com>
2023-06-10 23:54:03 +0330

    ✨ feat: added about and contact_us

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from portfolio.pages.views import HomeView, AboutView
+from portfolio.pages.views import HomeView, AboutView, ContactUsView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('home/', HomeView.as_view()),
-    path('about/', AboutView.as_view(), name='about'),
+    path('about-us/', AboutView.as_view(), name='about'),
+    path('contact-us/', ContactUsView.as_view(), name='contact_us'),
 ]
