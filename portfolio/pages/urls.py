b876f0f4e5e4bac01ba97386613f338005fb029f
Reza shakeri <rzashakeri@gmail.com>
2023-06-10 06:00:23 +0330

    ♻️ refactor: added new home url

--- portfolio/pages/urls.py
+++ portfolio/pages/urls.py
@@ -4,5 +4,6 @@ from portfolio.pages.views import HomeView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('home/', HomeView.as_view()),
     path('about/', AboutView.as_view(), name='about'),
 ]
