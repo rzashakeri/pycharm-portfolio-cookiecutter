f254608e34e1b080acb157a0e890a5b498b14595
Reza shakeri <rzashakeri@gmail.com>
2023-06-10 04:59:41 +0330

    ✨ feat: added SideBarView and render_navbar_title

--- portfolio/pages/views.py
+++ portfolio/pages/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 from portfolio.pages.models import SiteSettings, Page, About
 
 
 class HomeView(View):
     def get(self, request):
-        page = Page.objects.get(title="index.html")
-        portfolio = SiteSettings.objects.first()
+        home = Page.objects.get(slug="home")
         context = {
-            "page": page,
-            "portfolio": portfolio
+            "page": home,
         }
         return render(request, "pages/index.html", context=context)
 
 
 class AboutView(View):
     def get(self, request):
-        page = Page.objects.get(title="about.html")
-        portfolio = SiteSettings.objects.first()
         about = About.objects.first()
         context = {
-            "page": page,
-            "portfolio": portfolio,
-            "about": about
+            "page": about
         }
         return render(request, "pages/about.html", context=context)
+
+
+class SideBarView(TemplateView):
+    template_name = "shared/sidebar.html"
+    ordering = ['is_parent']
+
+    def get_context_data(self, **kwargs):
+        pages = Page.objects.all().order_by("-is_parent", "title")
+        kwargs['pages'] = pages
+        return super(SideBarView, self).get_context_data(**kwargs)
+
+
+def render_navbar_title(request):
+    portfolio = SiteSettings.objects.first()
+    context = {
+        "portfolio": portfolio
+    }
+    return render(request, "shared/partials/navbar_title.html", context=context)
