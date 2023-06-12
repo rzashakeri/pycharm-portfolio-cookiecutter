511228f45fe18ff02122db62862eb0c5b9978736
Reza shakeri <rzashakeri@gmail.com>
2023-06-12 10:51:45 +0330

    ✨ feat: added SocialMedia to admin.py

--- portfolio/pages/admin.py
+++ portfolio/pages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from portfolio.pages.models import Page, About, SkillCategory, Skill, ContactUs, Project, SiteSettings
+from portfolio.pages.models import Page, About, SkillCategory, Skill, ContactUs, Project, SiteSettings, SocialMedia
 
 admin.site.register(SiteSettings)
 
@@ -13,3 +13,4 @@ admin.site.register(SkillCategory)
 admin.site.register(Skill)
 admin.site.register(ContactUs)
 admin.site.register(Project)
+admin.site.register(SocialMedia)
