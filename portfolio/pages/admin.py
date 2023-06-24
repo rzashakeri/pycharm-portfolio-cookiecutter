850e484af29d59625255be3d3f68415644e88811
Reza shakeri <rzashakeri@gmail.com>
2023-06-24 20:14:10 +0330

    ✨ feat: added Education model to admin.py

--- portfolio/pages/admin.py
+++ portfolio/pages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from portfolio.pages.models import Page, About, SkillCategory, Skill, ContactUs, Project, SiteSettings, SocialMedia
+from portfolio.pages.models import Page, About, SkillCategory, Skill, ContactUs, Project, SiteSettings, SocialMedia, Education
 
 admin.site.register(SiteSettings)
 
@@ -14,3 +14,4 @@ admin.site.register(Skill)
 admin.site.register(ContactUs)
 admin.site.register(Project)
 admin.site.register(SocialMedia)
+admin.site.register(Education)
